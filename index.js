9f539071335ec16332dacabb2073a0304b15c5e3
maneesha.s <maneesha.s@objectstream.com>
2021-06-05 22:33:40 +0530

    test fix heroku

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ app.use('/',indexRouter)
 // app.get('/home', (req, res, next) => {
 // 	res.render('home', null)
 // })
-app.listen(3000)
-console.log('server running in http://localhost/3000')
+app.listen(process.env.PORT || 3000)
+console.log('server running in http://localhost:3000')
